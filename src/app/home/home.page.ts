69ecc68b824f6bc3fa82f13cc58cf0c2c3dd6b03
Charles Souza <charlestark00@gmail.com>
2022-10-06 22:30:06 -0300

    Criação de rotas com métodos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
+  constructor(public router: Router) {}
+
+  pagNgif() {
+    console.log('Acesso minha rota ngIf');
+    this.router.navigate(['/ngif']);
+  }
+  pagNgfor() {
+    console.log('Acessando minha rota ngfor');
+    this.router.navigate(['/ngfor']);
+  }
+  pagNgifContac() {
+    this.router.navigate(['/ngifcontatos']);
+  }
 }
